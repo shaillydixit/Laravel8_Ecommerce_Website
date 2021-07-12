5883f2abd4846f66b977267f6e6d8a9a0c8a7de3
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-07-13 05:25:57 +0530

    new product categorywise data loaded from backend with empty function

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1005,11 +1005,74 @@
 
                         <!-- /.tab-pane -->
 
+                        @foreach($categories as $category)
+                        <div class="tab-pane" id="category{{$category->id}}">
+                            <div class="product-slider">
+                                <div class="owl-carousel home-owl-carousel custom-carousel owl-theme" data-item="4">
+                                    @php
+                                    $catwiseProduct = App\Models\Product::where('category_id', $category->id)->orderBy('id', 'DESC')->get();
+                                    @endphp
 
-                        ======
 
+                                    @forelse($catwiseProduct as $product)
+                                    <div class="item item-carousel">
+                                        <div class="products">
+                                            <div class="product">
+                                                <div class="product-image">
+                                                    <div class="image"> <a href="detail.html"><img src="{{asset($product->product_thumbnail)}}" alt=""></a> </div>
+                                                    <!-- /.image -->
 
+                                                    <div class="tag new"><span>new</span></div>
+                                                </div>
+                                                <!-- /.product-image -->
 
+                                                <div class="product-info text-left">
+                                                    <h3 class="name"><a href="detail.html">{{$product->product_name_en}}</a></h3>
+                                                    <div class="rating rateit-small"></div>
+                                                    <div class="description"></div>
+                                                    <div class="product-price"> <span class="price"> $450.99 </span> <span class="price-before-discount">$ 800</span> </div>
+                                                    <!-- /.product-price -->
+
+                                                </div>
+                                                <!-- /.product-info -->
+                                                <div class="cart clearfix animate-effect">
+                                                    <div class="action">
+                                                        <ul class="list-unstyled">
+                                                            <li class="add-cart-button btn-group">
+                                                                <button data-toggle="tooltip" class="btn btn-primary icon" type="button" title="Add Cart"> <i class="fa fa-shopping-cart"></i> </button>
+                                                                <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+                                                            </li>
+                                                            <li class="lnk wishlist"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
+                                                            <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
+                                                        </ul>
+                                                    </div>
+                                                    <!-- /.action -->
+                                                </div>
+                                                <!-- /.cart -->
+                                            </div>
+                                            <!-- /.product -->
+
+                                        </div>
+                                        <!-- /.products -->
+                                    </div>
+                                    <!-- /.item -->
+                                    <!-- when no product available in category -->
+                                    @empty
+                                    <h5 class="text-danger">
+                                        No Product Found
+                                    </h5>
+                                    <!-- when no product available in category -->
+
+                                    @endforelse
+                                </div>
+                                <!-- /.item -->
+                            </div>
+                            <!-- /.home-owl-carousel -->
+                        </div>
+                        <!-- /.product-slider -->
+
+                        <!-- /.tab-pane -->
+                        @endforeach
 
 
 
